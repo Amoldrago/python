13933e98b9aeb13f8d6dbfa0dadb5d3ad80db012
Amol Drago <>
2023-11-04 16:47:54 +0530

    sample loan approval code

--- first.py
+++ first.py
@@ -1,1 +1,19 @@
-print("hello)
+print("hello")
+
+
+print("Load approval")
+price = input("Enter the load amount you are looking for ")
+cred_score = input("Enter your current cred score ")
+cred = int(cred_score)
+if cred < 600:
+    down_payment = 0.5*float(price)
+    print("Your cred score is below average, so you need to pay below amount")
+    print(down_payment)
+elif cred>600 and cred<700:
+    down_payment = 0.2*float(price)
+    print("Your cred score is good, so you need to pay below amount")
+    print(down_payment)
+else:
+    down_payment = 0.1*float(price)
+    print("Your cred score is excellent, so you need to pay below amount")
+    print(down_payment)
